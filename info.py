ec86c796ccbc9f44d75e6223eeefef9ea4d880e6
100nuk <146798173+100nuk@users.noreply.github.com>
2023-10-13 13:25:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -53,7 +53,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', True))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shareus.io')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shrs.link/RGDtVf')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'x4ALmu9QKKeRueksak8f0NrqUct1')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
