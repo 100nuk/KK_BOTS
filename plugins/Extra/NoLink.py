330b4c0489caaefa2b62f5091fad293b1aadb316
100nuk <146798173+100nuk@users.noreply.github.com>
2024-01-24 14:49:32 +0530

    Update NoLink.py

--- plugins/Extra/NoLink.py
+++ plugins/Extra/NoLink.py
@@ -42,7 +42,7 @@ async def nolink(bot,message):
 	try:
 
 	        if await is_check_admin(client, message.chat.id, message.from_user.id):
-                return
+                    return
                 hmm = await message.delete()
                 return await message.reply('Links not allowed here!')
                 
