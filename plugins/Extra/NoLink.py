ace4cc5d6e11bb619a8418044e285bb8f410519a
100nuk <146798173+100nuk@users.noreply.github.com>
2024-03-05 13:04:05 +0530

    Update NoLink.py

--- plugins/Extra/NoLink.py
+++ plugins/Extra/NoLink.py
@@ -39,7 +39,8 @@ Bot = Client(
 @Client.on_message((filters.group) & filters.regex("@")  | filters.regex("t.me") | filters.regex("https") | filters.regex("www.") | filters.regex("porn"))
 async def nolink(bot,message):
         
-        try:
+        if await is_check_admin(client, message.chat.id, message.from_user.id):
+                return
 		
             hmm = await message.delete()
             return await message.reply('Links not allowed here!')
